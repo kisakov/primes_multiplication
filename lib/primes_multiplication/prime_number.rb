51b9653fc72a628fb15ae7c93159aa58db5b2ac2
kisakov <isakov90@gmail.com>
2017-05-16 18:26:21 -0700

    implement PrimeNumber#first

--- lib/primes_multiplication/prime_number.rb
+++ lib/primes_multiplication/prime_number.rb
@@ -10,7 +10,21 @@ module PrimesMultiplication
         coeff.all? { |n| n % p == 0 }
       end
 
+      def first(n)
+        sequence.take(n)
+      end
+
       private
+
+      def sequence
+        Enumerator.new do |y|
+          n = 2
+          loop do
+            y << n if prime?(n)
+            n += 1
+          end
+        end
+      end
 
       def x_minus_1_to_the(p)
         p.times.inject([1]) do |ex, _|
